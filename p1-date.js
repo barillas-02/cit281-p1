67b8c1a416f8a4aae4024c2541e0aadbfad05299
barillas-02 <barillas@uoregon.edu>
2025-06-08 22:35:17 -0700

    Create p1-date.js

---
+++ p1-date.js
@@ -0,0 +1,9 @@
+/*
+    CIT 281 Project 1
+    Name: Isabella Barillas
+*/
+
+const today = new Date();
+const days = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"]
+const dayIndex = today.getDay()
+console.log(days[dayIndex]);
